77e7698124fdad9f5ba867b3af133eb35c325b69
Aline Amano <aline.amano95@gmail.com>
2025-07-25 20:45:51 -0300

    fix(wasm): ensure proper initialization and exports for WebAssembly module

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,9 +3,10 @@
 emcc src/main.cpp -o dist/compress.js \
   -O3 \
   -s WASM=1 \
-  -s EXPORTED_FUNCTIONS='["_compressImageToJpg", "_freeCompressedImage"]' \
-  -s EXPORTED_RUNTIME_METHODS='["cwrap", "getValue", "setValue"]' \
+  -s EXPORTED_FUNCTIONS='["_compressImageToJpg", "_freeCompressedImage",  "_malloc", "_free"]' \
+  -s EXPORTED_RUNTIME_METHODS='["cwrap", "getValue", "setValue", "HEAPU8", "HEAP32"]' \
   -s ALLOW_MEMORY_GROWTH=1 \
   -s MODULARIZE=1 \
+  -s EXPORT_ES6=1 \
   -s EXPORT_NAME='createModule' \
   -I src/
